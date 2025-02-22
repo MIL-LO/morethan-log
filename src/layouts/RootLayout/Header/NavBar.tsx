1df081ad71a2a0d3e0436fe3106daf3d971f5b97
악덕 <rudejrqkr97@gmail.com>
2025-02-22 19:54:42 +0900

    feat(Header): 네비게이션 바 링크 수정
    - "About" 링크를 "팀원소개"로 변경
    - 링크의 대상 URL을 "/about"에서 "/teams"로 변경

--- src/layouts/RootLayout/Header/NavBar.tsx
+++ src/layouts/RootLayout/Header/NavBar.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled"
 import Link from "next/link"
 
 const NavBar: React.FC = () => {
-  const links = [{ id: 1, name: "About", to: "/about" }]
+  const links = [{ id: 1, name: "팀원소개", to: "/teams" }]
   return (
     <StyledWrapper className="">
       <ul>
